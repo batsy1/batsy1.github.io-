7bbcbca7b81f701b3083424ee6912b43103a8a49
batsy1 <37593205+batsy1@users.noreply.github.com>
2018-06-04 12:36:19 +0500

    fgbnhjymunyh
    btпамквмпирторрррррр

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	
+</head>
+<body>
+<p>PHP test</p>
+  <?php  echo "Hello world!" ;  ?>
+  <p>Пробую сделать на русском</p>
+  <?php
+
+$name = "I like PHP!";
+$txt1 = "Learn PHP";
+$txtName1 = "Получится ли на русском?";
+$x = 4;
+  print "<h1>" .$txt1. "</h1>";
+  print "<h1>" . $name . "</h1>";
+  print "<h1>" . $txtName1 . "</h1>";
+  print $name + $txt1;
+  print $x + $x;
+  ?> 
+</body>
+</html>
